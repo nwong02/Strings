29f0885823535f41881cdc3fb8e1f2c2e9831545
Nelson Wong <nelsonwong107@gmail.com>
2021-10-09 14:52:05 -0400

    test

--- StringSample.cpp
+++ StringSample.cpp
@@ -8,11 +8,25 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	char ourString[10] = { 0 };
+	char c1, c2, c3, c4;
 	int index = 0;
+
+
+	cout << "Enter a line of input: \n";
+	cin.get(c1);
+	cin.get(c2);
+	cin.get(c3);
+	cin.get(c4);
+	cout << c1 << c2 << c3 << c4 << "END OF OUTPUT";
+
 	while (ourString[index] != '/0')
 	{
 		ourString[index] = 'X';
 		index++;
 	}
+
+
+
+
 	return 1;
 }
